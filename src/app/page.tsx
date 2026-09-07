a06cbf5b2a4696809a513493db6b2f5bf437f3a7
Bimo Satriantoro_088700 <91940505+bimomobile@users.noreply.github.com>
2026-09-07 08:54:34 +0700

    forget push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,20 @@ import { StatisticsBar } from "@/presentation/components/home/StatisticsBar";
 import { RecommendationSection } from "@/presentation/components/home/RecommendationSection";
 import { ValueStrip } from "@/presentation/components/home/ValueStrip";
 import { GetRecommendedProperties } from "@/application/use-cases/GetRecommendedProperties";
-import { GetStatistics } from "@/application/use-cases/GetStatistics";
 import { container } from "@/infrastructure/di/container";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   const getProperties = new GetRecommendedProperties(container.propertyRepo);
-  const getStats = new GetStatistics(container.statisticRepo);
 
-  const [result, stats] = await Promise.all([
-    getProperties.execute(),
-    getStats.execute(),
-  ]);
+  const result = await getProperties.execute();
 
   return (
     <div className="flex w-full flex-col bg-background pt-16 md:pt-20">
       <HeroSection />
       <HighlightCards />
-      <StatisticsBar stats={stats} />
+      <StatisticsBar />
       <RecommendationSection properties={result.properties} salesMap={result.salesMap} />
       <ValueStrip />
     </div>
